af84580475ad330b808b0b07e9f666183301be19
GHKyrie <nemasterya@gmail.com>
2021-03-06 15:48:58 +0500

    #96 dimych

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,9 +1,10 @@
 import {profileAPI, usersAPI} from "../api/api";
 
-const ADD_POST = 'ADD-POST';
-const SET_USER_PROFILE = 'SET_USER_PROFILE';
-const SET_STATUS = 'SET_STATUS';
-const DELETE_POST = 'DELETE_POST';
+const ADD_POST = 'PR_ADD-POST';
+const SET_USER_PROFILE = 'PR_SET_USER_PROFILE';
+const SET_STATUS = 'PR_SET_STATUS';
+const DELETE_POST = 'PR_DELETE_POST';
+const SAVE_PHOTO_SUCCESS = 'PR_SAVE_PHOTO_SUCCESS';
 
 let initialState = {
     posts: [
@@ -41,6 +42,9 @@ const profileReducer = (state = initialState, action) => {
         case DELETE_POST: {
             return {...state, posts: state.posts.filter(p => p.id != action.postId)}
         }
+        case SAVE_PHOTO_SUCCESS: {
+            return {...state, profile: {...state.profile, photos: action.photos} }
+        }
         default:
             return state;
     }
@@ -50,6 +54,7 @@ export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostTe
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const setStatus = (status) => ({type: SET_STATUS, status})
 export const deletePost = (postId) => ({type: DELETE_POST, postId})
+export const savePhotoSuccess = (photos) => ({type: SAVE_PHOTO_SUCCESS, photos})
 
 
 export const getUserProfile = (userId) => async (dispatch) => {
@@ -66,6 +71,13 @@ export const updateStatus = (status) => async (dispatch) => {
         if (response.data.resultCode === 0) {
             dispatch(setStatus(status));
         }
+}
+
+export const savePhoto = (file) => async (dispatch) => {
+    let response = await profileAPI.savePhoto(file)
+    if (response.data.resultCode === 0) {
+        dispatch(savePhotoSuccess(response.data.data.photos));
+    }
 }
 
 export default profileReducer;
